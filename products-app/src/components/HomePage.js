f55ffa3b6bcffc9afbf3bb924e7133e95813381e
nerdlan <realnerdlan@gmail.com>
2024-03-13 17:14:15 -0600

    Change names in HomePage.js

--- products-app/src/components/HomePage.js
+++ products-app/src/components/HomePage.js
@@ -38,11 +38,11 @@ const HomePage = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td style={styles.td}>A B</td>
+                        <td style={styles.td}>Nolan Thomas </td>
                         <td style={styles.td}>Developer</td>
                     </tr>
                     <tr>
-                        <td style={styles.td}>C D</td>
+                        <td style={styles.td}>Nathan Mangold</td>
                         <td style={styles.td}>Designer</td>
                     </tr>
                 </tbody>
